c51407f347520c6e0198d44bdf4285f8e3de4d57
Jeffrey Schutzman <jeff@pyebrook.com>
2014-10-17 17:09:54 -0400

    changed import added screenshot

--- functions.php
+++ functions.php
@@ -21,6 +21,13 @@
 */
 
 
+add_action( 'wp_enqueue_scripts', 'enqueue_child_theme_styles', PHP_INT_MAX);
+function enqueue_child_theme_styles() {
+	wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+	wp_enqueue_style( 'child-style', get_stylesheet_uri(), array('parent-style')  );
+}
+
+
 add_filter( 'wpsc_get_customer_meta', 'force_grid_view', 10, 3 );
 
 function force_grid_view( $check_value, $key, $id ) {
